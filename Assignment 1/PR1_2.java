36ffa10fb8e889f90276b6fe9b7cf781143e6d29
abhi-rupareliya <96171169+abhi-rupareliya@users.noreply.github.com>
2022-07-31 12:52:10 +0530

    Create PR1_2.java

---
+++ Assignment 1/PR1_2.java
@@ -0,0 +1,16 @@
+public class PR1_2 {
+    public void startOz(String s) {
+        if (s.charAt(0) == 'o') {
+            if (s.charAt(1) == 'z') {
+                System.out.println("oz");
+            } else {
+                System.out.println("o");
+            }
+        }
+        else{
+            if(s.charAt(1)=='z'){
+                System.out.println("z");
+            }
+        }
+    }
+}
