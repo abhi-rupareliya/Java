d11737a7437c256b238ff3688c4a7d25f94d2c17
abhi-rupareliya <96171169+abhi-rupareliya@users.noreply.github.com>
2022-10-09 22:25:01 +0530

    Add files via upload

---
+++ Practical Assignment/PR11.java
@@ -0,0 +1,32 @@
+class Triplet<T> {
+    private T object1;
+    private T object2;
+    private T object3;
+    
+    public Triplet(T object1, T object2, T object3) {
+        this.object1 = object1;
+        this.object2 = object2;
+        this.object3 = object3;
+    }
+    
+    public T getObject1() {
+        return object1;
+    }
+    
+    public T getObject2() {
+        return object2;
+    }
+    
+    public T getObject3() {
+        return object3;
+    }
+}
+
+public class PR11 {
+    public static void main(String[] args) {
+        Triplet<Integer> triplet = new Triplet<>(10,20,30);
+        System.out.println(triplet.getObject1());
+        System.out.println(triplet.getObject2());
+        System.out.println(triplet.getObject3());
+    }
+}
